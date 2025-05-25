115f52a73be7f8c1fab26492d9a3e347fc4c4712
P-Worthington <pworthington17@gmail.com>
2025-05-25 20:03:30 +0100

    clean code

--- main.py
+++ main.py
@@ -15,5 +15,4 @@ def check_number():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    # Listen on all interfaces on the correct port for Cloud Run
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
