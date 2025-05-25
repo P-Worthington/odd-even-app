037955a1189aa76cdec87fe9fdec250a87885ddb
P-Worthington <pworthington17@gmail.com>
2025-05-25 19:28:50 +0100

    Fix app to listen on 8080

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def check_number():
+    # Your odd/even logic here
+    return "Hello from odd-even app!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 @app.route('/')
 def check_number():
