1c1df4a3aa15180be2db93d14cd75b197e1cc805
P-Worthington <pworthington17@gmail.com>
2025-05-25 16:44:33 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def check_number():
+    num = request.args.get('number', type=int)
+    if num is None:
+        return "Please provide a number in the URL like ?number=4"
+    if num % 2 == 0:
+        return f"{num} is even."
+    else:
+        return f"{num} is odd."
+
+if __name__ == '__main__':
+    app.run(debug=True)
